833df79f0b6aa0324b4afcca9af078c77db0a10a
Rohith <rohith.krithivasan@gmail.com>
2021-12-23 21:54:55 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let answer = 0;
+
+function loadProblem() {
+    let element = document.getElementById("problem");
+    element.innerHTML="";
+    let numberOfNumbers = Math.floor(Math.random() * 10) + 2;
+    for(let i = 0; i < numberOfNumbers; i++) {
+        let temp = Math.floor(Math.random() * 100);
+        element.innerHTML += ("" + temp);
+        answer += temp;
+        if(i !== numberOfNumbers - 1) element.innerHTML += " + ";
+    }
+    console.log(answer);
+}
+
+function validateAnswer(userAnswer) {
+    console.log(`${answer}, ${userAnswer}`);
+    if(userAnswer == answer) {
+        alert("Correct!");
+        document.getElementById("answer").value = "";
+        loadProblem();
+        return false;
+    } else {
+        alert("Incorrect!");
+        return false;
+    }
+}
